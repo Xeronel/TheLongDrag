b34dd4bb10101128c957f2b5a81ca4f9775f649c
Ripster <ripster1121@gmail.com>
2018-06-14 14:43:50 -0400

    Get title from database or cache

--- lib/the_long_drag_web/views/layout_view.ex
+++ lib/the_long_drag_web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule TheLongDragWeb.LayoutView do
   use TheLongDragWeb, :view
+
+  def title do
+    Cachex.fetch(:tld_cache, "title", &commit_title/0)
+    |> title()
+  end
+
+  defp title({:ok, title}) do
+    title
+  end
+
+  defp title({:commit, title}) do
+    title
+  end
+
+  defp commit_title() do
+    {:commit, TheLongDrag.Schema.Site.get().title}
+  end
 end
